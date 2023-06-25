e0ed60dc3c7df421679a9edc025062f74f427f33
Matthew Bull <mattbull33@gmail.com>
2023-06-25 18:58:10 -0400

    models

---
+++ models/Thought.js
@@ -0,0 +1,40 @@
+const { Schema, model } = require('mongoose');
+const reactionSchema = require('./Reaction');
+
+// Schema to create new thought
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280,
+        },
+        createdAt: {
+            type: Date,
+            required: true,
+            default: Date.now(),
+        },
+        username: {
+            type: String,
+            requited: true
+        },
+        reactions: [
+            reactionSchema
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
