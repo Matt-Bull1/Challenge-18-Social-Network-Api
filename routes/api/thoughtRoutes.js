d136301890676d9b93750f21d3369b510f233ffd
Matthew Bull <mattbull33@gmail.com>
2023-06-25 19:29:23 -0400

    .

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,31 +1,21 @@
 const router = require('express').Router();
-const { getThoughts, 
-        createThought, 
-        getSingleThought, 
-        updateThought,
-        deleteThought,
-        createReaction,
-        deleteReaction 
-    } = require('../../controllers/thoughtController');
+const { } = require('../../controllers/thoughtController');
 
 
 //api/thoughts
 router.route('/')
-    .get(getThoughts)
-    .post(createThought);
+
 
 //api/thoughts
 router.route('/:thoughtId')
-    .get(getSingleThought)
-    .put(updateThought)
-    .delete(deleteThought);
+
 
 //api/thoughts
 router.route('/:thoughtId/reactions')
-    .post(createReaction);
+
 
 //api/thoughts
 router.route('/:thoughtId/reactions/:reactionId')
-    .delete(deleteReaction);
+
 
 module.exports = router;
