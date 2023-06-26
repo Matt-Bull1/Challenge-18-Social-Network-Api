66fe8e33eba7ccee61288090fc2f0d7815f17175
Matthew Bull <mattbull33@gmail.com>
2023-06-25 22:32:54 -0400

    controllers added

--- controllers/userController.js
+++ controllers/userController.js
@@ -68,7 +68,7 @@ module.exports = {
         return res.status(404).json({ message: "No user with that ID" });
       }
 
-      res.json({ message: "User successfully deleted" });
+      res.json({ message: "User deleted" });
     } catch (err) {
       console.log(err);
       res.status(500).json(err);
